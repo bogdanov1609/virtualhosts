4aa5190aa285213a4d5c8687d4fee0fd961f4b6a
V Grozin <vladislav.grozin@yandex.ru>
2014-07-02 16:27:58 -0400

    'Enabled' flags added

--- update.py
+++ update.py
@@ -21,24 +21,26 @@ def runscript(script):
     return output
 
 
-def sqlcheck(con, username, sqlpass):
-    cur.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s'" % username)
-    rows = cur.fetchone()
+def sqlcheck(con, acc):
+    con["vhosts"].execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s'" % acc["name"])
+    rows = con["vhosts"].fetchone()
     if rows[0] != 1:
-        cur.execute("CREATE DATABASE %s" % username)
-        cur.execute("GRANT ALL PRIVILEGES ON %s.* TO '%s'@'localhost' IDENTIFIED BY '%s'" % (username, username, sqlpass))
-        cur.execute("FLUSH PRIVILEGES")
-        return "SQL DB %s created" % username
+        con["vhosts"].execute("CREATE DATABASE %s" % acc["name"])
+        con["vhosts"].execute("GRANT ALL PRIVILEGES ON %s.* TO '%s'@'localhost' IDENTIFIED BY '%s'" % (acc["name"], acc["name"], acc["SQLpass"]))
+        con["vhosts"].execute("FLUSH PRIVILEGES")
+        return "SQL DB %s created" % acc["name"]
     return ""
 
 
-def usercheck(username, ftppass, userroot):
+def usercheck(con, acc):
     out = ""
+    username = acc["name"]
+    userroot = acc["root"]
     try:
         pwd.getpwnam(username)
     except KeyError:
         runscript("groupadd %s" % username)
-        runscript("useradd -g %s -p %s %s" % (username, ftppass, username))
+        runscript("useradd -g %s -p %s %s" % (username, acc["FTPpass"], username))
         out += "Creating user for %s" % username
     if not os.path.isdir(userroot):
         os.makedirs(userroot+"/htdocs")
@@ -58,26 +60,24 @@ def usercheck(username, ftppass, userroot):
     return out
 
 
-def ftpcheck(sqlcur, username, password, userroot):
-    encPass = crypt.crypt(password, "22")
-    sqlcur.execute("SELECT COUNT(*) FROM ftpuser WHERE userid='%s'" % username)
-    count = sqlcur.fetchone()
+def ftpcheck(con, acc):
+    encPass = crypt.crypt(acc["FTPpass"], "22")
+    con["ftp"].execute("SELECT COUNT(*) FROM ftpuser WHERE userid='%s'" % acc["name"])
+    count = con["ftp"].fetchone()
     if count[0] != 1:
-        sqlcur.execute("INSERT INTO  `ftpd`.`ftpuser` (`userid`,`passwd`,`homedir`) VALUES ('%s', '%s', '%s/htdocs');" % (username, encPass, userroot))
-        return "FTP user %s created" % username
+        con["ftp"].execute("INSERT INTO  `ftpd`.`ftpuser` (`userid`,`passwd`,`homedir`) VALUES ('%s', '%s', '%s/htdocs');" % (acc["name"], encPass, acc["root"]))
+        return "FTP user %s created" % acc["name"]
     else:
         return ""
 
 
-def apachecheck(username, hostname, root, alias, apache):
-    confname = "%s_%s.conf" % (hostname, username)
-    filename = "%s/%s" % (apache, confname)
+def apachecheck(con, acc):
+    confname = "%s_%s.conf" % (acc["hostname"], acc["name"])
+    filename = "%s/%s" % (con["apache"], confname)
     cfg = open("template.conf", 'r').read()
     if not os.path.isfile(filename):
-        if alias != "":
-            alias = "ServerAlias %s" % alias
-        cfg = cfg.replace("%USERNAME%", username).replace("%HOSTNAME%", hostname)
-        cfg = cfg.replace("%ROOT%", root).replace("%ALIAS%", alias)
+        cfg = cfg.replace("%USERNAME%", acc["name"]).replace("%HOSTNAME%", acc["hostnames"])
+        cfg = cfg.replace("%ROOT%", acc["root"]).replace("%CUSTOM%", acc["custom"])
         cfile = open(filename, 'w')
         cfile.write(cfg)
         cfile.close()
@@ -86,8 +86,8 @@ def apachecheck(username, hostname, root, alias, apache):
         return ""
 
 
-def hostscheck(username):
-    sqlname = username+".mysqlserver"
+def hostscheck(con, acc):
+    sqlname = acc["name"]+".mysqlserver"
     with open("/etc/hosts") as hosts:
         exists = False
         for line in hosts:
@@ -103,41 +103,71 @@ def hostscheck(username):
     return "Created sqlserver alias %s" % sqlname
 
 
-if __name__ == "__main__":
-    print "Script started at " + time.ctime()
+def checkaccount(con, acc):
+    if acc["root"] == "":
+        acc["root"] = con["root"] + acc["hostname"]
+        con["vhosts"].execute("UPDATE vhosts SET root='%s' WHERE id=%s" % (acc["root"], acc["id"]))
+        log(acc["name"], "Setting %s's root to %s" % (acc["name"], acc["root"]))
+
+    log(acc["name"], usercheck(con, acc))
+    if (acc["SQLenabled"]!=0):
+        log(acc["name"], sqlcheck(con, acc))
+        log(acc["name"], hostscheck(con, acc))
+    if (acc["FTPenabled"]!=0):
+        log(acc["name"], ftpcheck(con, acc))
+    if (acc["ApacheEnabled"]!=0):
+        log(acc["name"], apachecheck(con, acc))
+
+
+def readconfig(conf):
+    con = {}
     cfg = ConfigParser.ConfigParser()
-    cfg.read("config.cfg")
-    db = MySQLdb.connect(host=cfg.get("main", "host"),
-    user=cfg.get("main", "user"),
-    passwd=cfg.get("main", "passwd"),
-    db=cfg.get("main", "db"))
-    ftpdb = MySQLdb.connect(host=cfg.get("main", "host"),
-    user=cfg.get("main", "user"),
-    passwd=cfg.get("main", "passwd"),
-    db=cfg.get("main", "ftpdb"))
-    defaultroot = cfg.get("main", "root")
-    apachedir = cfg.get("main", "apachedir")
+    cfg.read(conf)
+    con["db"] = MySQLdb.connect(host=cfg.get("main", "host"),
+        user=cfg.get("main", "user"),
+        passwd=cfg.get("main", "passwd"),
+        db=cfg.get("main", "db"))
+    con["ftpdb"] = MySQLdb.connect(host=cfg.get("main", "host"),
+        user=cfg.get("main", "user"),
+        passwd=cfg.get("main", "passwd"),
+        db=cfg.get("main", "ftpdb"))
+    con["root"] = cfg.get("main", "root")
+    con["apache"] = cfg.get("main", "apachedir")
+    
+    con["ftp"] = con["ftpdb"].cursor()
+    con["vhosts"] = con["db"].cursor()    
+    return con
 
-    ftpcur = ftpdb.cursor()
-    cur = db.cursor()
 
-    cur.execute("SELECT COUNT(*) FROM vhosts")
-    count = cur.fetchone()
+def hello(con):
+    print "Script started at " + time.ctime()
+    con["vhosts"].execute("SELECT COUNT(*) FROM vhosts")
+    count = con["vhosts"].fetchone()
     print "Vhosts: %s" % (count[0])
 
-    cur.execute("SELECT id, username, hostname, alias, SQLpass, FTPpass, root FROM vhosts")
-    rows = cur.fetchall()
-    for row in rows :
-        id, name, hname, alias, sqlp, ftpp, root = row
-        if root == "":
-            root = defaultroot + hname
-            cur.execute("UPDATE vhosts SET root='%s' WHERE id=%s" % (root, id))
-            log(name, "Setting %s's root to %s" % (name, root))
-        log(name, usercheck(name, ftpp, root))
-        log(name, sqlcheck(cur, name, sqlp))
-        log(name, ftpcheck(ftpcur, name, ftpp, root))
-        log(name, apachecheck(name, hname, root, alias, apachedir))
-        log(name, hostscheck(name))
-    db.commit()
+
+def getaccounts(con):
+    con["vhosts"].execute("SELECT id, name, hostnames, custom, SQLpass, FTPpass, root, FTPenabled, SQLenabled, ApacheEnabled FROM vhosts WHERE enabled=1")
+    accs = []
+    columns = tuple( [d[0] for d in con["vhosts"].description] )
+    for row in con["vhosts"]:
+        #User can have multuple host names; let's get the first one
+        acc = dict(zip(columns, row))
+        acc["hostname"] = acc["hostnames"].split()[0]
+        accs.append(acc)
+    return accs
+
+
+def finish(con):
+    con["db"].commit()
     print "Done; restarting apache"
-    print runscript("service apache2 restart")
+    runscript("service apache2 restart")
+
+
+if __name__ == "__main__":
+    con = readconfig("config.cfg")
+    hello(con)
+    accs = getaccounts(con)
+    for acc in accs:
+        checkaccount(con, acc)
+    finish(con)
